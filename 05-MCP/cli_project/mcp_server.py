cf72b72f1cfbded4b23caa9b7ac3126297e87a97
Huda <huda.tariq94@gmail.com>
2026-06-30 13:29:26 +0400

    Added list, get individual prompts in mcp client along with reading resources + Added resources to fetch and list the documents with prompts to format in markdown format in mcp_server

--- 05-MCP/cli_project/mcp_server.py
+++ 05-MCP/cli_project/mcp_server.py
@@ -1,5 +1,6 @@
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
+from mcp.server.fastmcp.prompts import base
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
@@ -13,7 +14,6 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a tool to read a doc
 @mcp.tool(
     name="read_doc_documents",
     description="Read the contents of the document and return it as a string"
@@ -23,7 +23,7 @@ def read_documents(doc_id:str=Field(description="The id of the document to read"
     if doc_id not in docs:
         raise ValueError(f"Document {doc_id} not found")
     return docs[doc_id]
-# TODO: Write a tool to edit a doc
+
 @mcp.tool(
     name="edit_doc_documents",
     description="Edit a document by replacing a string in the documents with a new string"  
@@ -37,9 +37,44 @@ def edit_documents(
         raise ValueError(f"Document {doc_id} not found")
     docs[doc_id] = docs[doc_id].replace(old_string, new_string)
     return docs[doc_id]
-# TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
-# TODO: Write a prompt to rewrite a doc in markdown format
+
+
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json",
+)
+def list_docs() -> list[str]:
+    return list(docs.keys())
+
+
+@mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type="text/plain"
+)
+def fetch_doc(doc_id:str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document {doc_id} not found")
+    return docs[doc_id]
+
+
+@mcp.prompt(
+    name="format",
+    description="Rewrites the contents of a document in Markdown Format"
+)
+def format_doc(doc_id:str = Field(description="Id of the document to format")) -> list[base.Message]:
+    prompt= f"""
+    Your goal is reformat a document to be written with markdown syntax.
+
+    The id of the document you need to format is:
+    <document_id>
+    {doc_id}
+    </document_id>
+
+    Add in headers, bullet points, tables etc as necessary. Feel free to add in extra formatting.
+    Use the 'edit_document' tool to edit the document. After the document has been reformatted.
+    """
+   
+    return [base.UserMessage(prompt)]
 # TODO: Write a prompt to summarize a doc
 
 
